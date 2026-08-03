636bf9be616e85a1d5367e5f6d279354ec0754e5
gijaeAhn <125946077+gijaeAhn@users.noreply.github.com>
2026-08-03 12:01:00 +0000

    Deploying to gh-pages from @ gijaeAhn/gijaeAhn.github.io@9f4bf766a369dc643509e1d18edcf3e0b1d032d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,13 +451,6 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/CV_2026_05_03.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
